3f2d7500a0407f0ab9cf15a7ffd1a8af09c6391f
TheNootLinja <thenootlinja@gmail.com>
2022-03-24 23:41:52 -0400

    Add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="h-12 w-full border-t border-zinc-800 fixed bottom-0 flex justify-around items-center">
+      <div className="bg-red-500 w-8 h-8"/>
+      <div className="bg-red-500 w-8 h-8"/>
+      <div className="bg-red-500 w-8 h-8"/>
+      <div className="bg-red-500 w-8 h-8"/>
+    </div>
+  )
+}
+
+export default Footer
